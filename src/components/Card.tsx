84ba15bc9e139cac1702686931ea8311cd35300f
NirmanStha <nirmans39@gmail.com>
2023-06-16 18:33:48 +0545

    fixed props passing issue

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,31 +1,38 @@
-
+import { useState } from 'react'
 import { Link } from "react-router-dom"
 import { MovieDetails } from "../types/movie"
 
 interface CardProps {
-  movieDetails: MovieDetails[]
+  movieDetails: MovieDetails[] | null;
+
+
 }
 
-const Card = ({ movieDetails }: CardProps) => (
-  <div className="card flex  mx-auto space-x-1 text-white w-full justify-center items-center">
-    {
-      movieDetails.map((movie) => (
-        <Link className=" rounded card min-h-64 max-w-xs" to={`/movie_details/${movie.id}`} key={movie.id}>
-          <div>
-            <div className="img h-4/5 border-white border-4 transition duration-700 ease-in-out hover:border-green  ">
+const Card = ({ movieDetails }: CardProps) => {
 
-              <img className="h-full w-full" src={movie.medium_cover_image} alt='Error loading image' />
-            </div>
-            <div className="description mt-1 ">
-              <h5 className="text-white font-bold">{movie.title.length > 20 ? (movie.title.substring(0, 20) + '....') : (movie.title)}</h5>
-              <p className="font-light">{movie.year}</p>
-            </div>
-          </div>
-        </Link >
-      ))
-    }
 
-  </div >
-)
+  return (
+    <div className="card flex  mx-auto space-x-1 text-white w-full justify-center items-center">
+      {
+        movieDetails != null ? (movieDetails.map((movie) => (
+          <Link className=" rounded card min-h-64 max-w-xs" to={`/movie_details/${movie.id}`} key={movie.id}>
+            <div>
+              <div className="img h-4/5 border-white border-4 transition duration-700 ease-in-out hover:border-green  ">
+
+                <img className="h-full w-full" src={movie.medium_cover_image} alt='Error loading image' />
+              </div>
+              <div className="description mt-1 ">
+                <h5 className="text-white font-bold">{movie.title.length > 20 ? (movie.title.substring(0, 20) + '....') : (movie.title)}</h5>
+                <p className="font-light">{movie.year}</p>
+              </div>
+            </div>
+          </Link >
+        ))) : ("Loading")
+
+
+      }
+
+    </div >)
+}
 
 export default Card
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,22 +8,26 @@ const Home = () => {
 
     const [popular, setPopular] = useState<MovieDetails[] | null>(null)
     const [latest, setLatest] = useState<MovieDetails[] | null>(null)
-    const [first, setfirst] = useState<MovieDetails[] | null>(null)
+    const [upComing, setUpComing] = useState<MovieDetails[] | null>(null)
 
     useEffect(() => {
         (async function () {
             const data = await fetchMovies(16);
-            setPopular()
+            setPopular(data.slice(0, 4));
+            setLatest(data.slice(4, 12));
+            setUpComing(data.slice(12, 16));
+
 
         })(); // tei call garney function
 
     }, [])
+    console.log(popular)
 
     return (
         <div className='container bg-prim mx-auto h-screen text-white '>
             <h2 className='text-center font-bold text-xxl m-2'>Popular Download</h2>
             <div className="card-container mx-auto p-2 popular">
-                {moviesDetails != null ? (<Card movieDetails={moviesDetails} />) : (<div> Fetchin datta</div>)}
+                <Card movieDetails={popular} />
             </div>
         </div>
     )
